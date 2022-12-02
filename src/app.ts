63e177f692b22d7013772b287fadc32cd5db5606
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-12-02 20:21:48 -0300

    Updates end

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(process.env.DB_PORT || 3003, () => {
+app.listen(3003, () => {
   console.log("Server is running in http://localhost:3003");
 });
 
