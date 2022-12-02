07b9c01baaf549234bea6ad544c2ad1e4df8af2c
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-12-02 14:02:56 -0300

    updates deploy

--- revisao-mod5-backend/src/app.ts
+++ revisao-mod5-backend/src/app.ts
@@ -1,9 +1,9 @@
 import express, { Express } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-import cors from "cors";
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
