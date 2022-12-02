f74cde21f8438e28bc384ac9b71395f22e1938be
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-12-02 19:21:47 -0300

    upadates installs

--- src/endpoints/createCharacter.ts
+++ src/endpoints/createCharacter.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
 import { connection } from "../connection";
+import {Request, Response} from "express";
 // import { characters } from "../data";
 
 export async function createCharacter(
